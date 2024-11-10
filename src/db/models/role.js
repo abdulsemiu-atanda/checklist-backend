dad2ed58accabc670159115485726d2cb462c902
Abdulsemiu Atanda <semiu@live.com>
2024-11-10 20:03:04 +0000

    migration and model definition for roles

---
+++ src/db/models/role.js
@@ -0,0 +1,33 @@
+import {Model} from 'sequelize'
+
+import {ADMIN, USER} from '../../config/roles'
+
+export default (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Role.init({
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.ENUM(ADMIN, USER)
+    }
+  }, {
+    sequelize,
+    modelName: 'Role',
+  })
+
+  return Role
+}
