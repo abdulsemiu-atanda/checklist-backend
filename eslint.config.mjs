4c32fd4230d589384f9187b8939e6d9c39bb6192
Abdulsemiu Atanda <semiu@live.com>
2024-11-20 21:43:47 +0000

    update eslint configuration and add new rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import jsdoc from 'eslint-plugin-jsdoc';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['src/app/**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {ignores: ['tests/*', 'src/db/models/index.js', 'src/db/migrations/*', 'src/db/seeders/*', '*.config.js', 'test.js']},
+  {files: ['src/app/**/*.js'], languageOptions: { sourceType: 'commonjs' }},
   {
     languageOptions: {
       globals: globals.node,
@@ -13,9 +13,14 @@ export default [
     },
   },
   pluginJs.configs.recommended,
-  jsdoc.configs['flat/recommended'],
   {
     rules: {
+      'comma-style': [2, 'last'],
+      'computed-property-spacing': [2, 'never'],
+      'eol-last': [2],
+      'func-style': [1, 'declaration', {allowArrowFunctions: true}],
+      indent: [2, 2, {MemberExpression: 1, SwitchCase: 1}],
+      semi: [2, 'never'],
       'no-unused-vars': 1,
       'no-undef': 0,
       'one-var': 0,
@@ -28,7 +33,7 @@ export default [
       'arrow-parens': 0,
       'import/no-dynamic-require': 0,
       'comma-dangle': 0,
-      curly: [2, 'multi-line'],
+      curly: [2, 'multi-or-nest', 'consistent'],
     },
   },
-];
+]
