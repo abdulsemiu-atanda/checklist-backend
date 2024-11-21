1c7f1cef9394966d4972f68b58b310bf0e35f1d3
Abdulsemiu Atanda <semiu@live.com>
2024-11-21 22:51:54 +0000

    add formatting to logger

--- src/app/constants/logger.js
+++ src/app/constants/logger.js
@@ -1,3 +1,6 @@
 import winston from 'winston'
 
-export default winston.createLogger({transports: [new winston.transports.Console()]})
+export default winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.simple()
+})
