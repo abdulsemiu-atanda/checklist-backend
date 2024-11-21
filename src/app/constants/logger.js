6e1e489e9b5f5b66501093f9462ef7008379ac46
Abdulsemiu Atanda <semiu@live.com>
2024-11-21 23:07:46 +0000

    set log level based on env

--- src/app/constants/logger.js
+++ src/app/constants/logger.js
@@ -1,6 +1,8 @@
 import winston from 'winston'
 
+const level = process.env.NODE_ENV === 'development' ? 'debug' : 'info'
+
 export default winston.createLogger({
-  transports: [new winston.transports.Console()],
+  transports: [new winston.transports.Console({level})],
   format: winston.format.simple()
 })
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import logger from './app/constants/logger'
 const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
+  logger.verbose('-'.repeat(80))
   logger.info(`Server is running on ${port}`)
+  logger.verbose('-'.repeat(80))
 })
 
 graceful({servers: [server], killTimeout: '15s'})
