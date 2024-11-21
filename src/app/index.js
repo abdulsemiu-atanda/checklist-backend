08e41a8f3222a5fbb6b652e6025dad27249c314b
Abdulsemiu Atanda <semiu@live.com>
2024-11-21 23:28:49 +0000

    list endpoints in dev mode

--- src/app/index.js
+++ src/app/index.js
@@ -4,12 +4,14 @@ import morgan from 'morgan'
 import express from 'express'
 
 import {dasherizeCamelCase} from '../util/tools'
+import logger from './constants/logger'
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.disable('x-powered-by')
 app.use(morgan('tiny'))
 
 fs.readdirSync(`${__dirname}/routes/`)
@@ -24,5 +26,18 @@ fs.readdirSync(`${__dirname}/routes/`)
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to Checklist API',
 }))
+
+if (process.env.NODE_ENV === 'development') {
+  const listRoutes = require('express-list-endpoints')
+  const Table = require('cli-table')
+
+  const routeList = listRoutes(app).map(route => ({[route.path]: route.methods.join(' | ')}))
+  const table = new Table()
+
+  table.push({Endpoints: 'Methods'}, ...routeList)
+
+  logger.verbose('-'.repeat(80))
+  logger.verbose(`THESE ARE THE AVAILABLE ENDPOINTS: \n${table.toString()}`)
+}
 
 export default app
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import logger from './app/constants/logger'
 const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
-  logger.verbose('-'.repeat(80))
   logger.info(`Server is running on ${port}`)
   logger.verbose('-'.repeat(80))
 })
