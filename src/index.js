bebb159447098ed5ff5f2c34cc5a9472045b1540
Abdulsemiu Atanda <semiu@live.com>
2024-11-21 22:52:11 +0000

    add graceful shutdown to server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import graceful from 'graceful'
+
 import app from './app'
+import logger from './app/constants/logger'
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`)
+const server = app.listen(port, () => {
+  logger.info(`Server is running on ${port}`)
 })
+
+graceful({servers: [server], killTimeout: '15s'})
