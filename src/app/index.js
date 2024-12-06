721d923410e81d9c4cd0b3cb386768b0ba44686b
Abdulsemiu Atanda <semiu@live.com>
2024-12-05 21:29:07 -0400

    list pending migrations if present

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,21 @@ import bodyParser from 'body-parser'
 import fs from 'fs'
 import morgan from 'morgan'
 import express from 'express'
+import Table from 'cli-table'
+import {Umzug, SequelizeStorage} from 'umzug'
 
-import {dasherizeCamelCase, smtpServer} from '../util/tools'
+import db from '../db/models'
+import {dasherizeCamelCase, isEmpty, smtpServer} from '../util/tools'
 import logger from './constants/logger'
 
-const emailSender = smtpServer()
 const app = express()
+const emailSender = smtpServer()
+const umzug = new Umzug({
+  migrations: {glob: `${__dirname}/../db/migrations/*.js`},
+  context: db.sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({sequelize: db.sequelize}),
+  logger: console
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -21,6 +30,20 @@ emailSender.transporter.verify(error => {
     logger.error(error.message)
   else
     logger.info('SMTP Server connection was successful.')
+})
+
+// Check pending migration and run migrations
+umzug.pending().then(migrations => {
+  if (isEmpty(migrations)) {
+    logger.info('Database is up to date with migrations.')
+  } else {
+    const collection = migrations.map(migration => ({[migration.name]: migration.path || 'N/A'}))
+    const table = new Table()
+
+    table.push({Name: 'Path'}, ...collection)
+
+    logger.verbose(`THESE ARE THE PENDING MIGRATIONS: \n${table.toString()}`)
+  }
 })
 
 fs.readdirSync(`${__dirname}/routes/`)
