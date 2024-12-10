38c0becfdade2cf01a430f72de4bf87e25769c17
Abdulsemiu Atanda <semiu@live.com>
2024-12-09 22:27:09 -0400

    update user model spec

--- tests/app/db/models/user.test.js
+++ tests/app/db/models/user.test.js
@@ -1,9 +1,11 @@
 import bcrypt from 'bcrypt'
 import {expect} from 'chai'
 
+import {dateToISOString} from '../../../../src/util/tools'
 import db from '../../../../src/db/models'
 import * as roleNames from '../../../../src/config/roles'
 import {fakeUser as attributes} from '../../../fixtures'
+import {smtpStub} from '../../../testHelpers'
 
 const Role = db.Role
 const User = db.User
@@ -38,6 +40,16 @@ describe('User Model:', () => {
       expect(record.email).to.equal(attributes.email.toLowerCase())
       expect(record.RoleId).to.equal(role.id)
       expect(bcrypt.compareSync(attributes.password, record.password)).to.equal(true)
+      expect(record.confirmed).to.equal(false)
+      expect(smtpStub.called).to.equal(true)
+
+      done()
+    })
+  })
+
+  it('returns correct value for confirmed user', done => {
+    user.update({confirmedAt: dateToISOString(Date.now())}).then(updated => {
+      expect(updated.confirmed).to.equal(true)
 
       done()
     })
