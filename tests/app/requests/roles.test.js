0febc9bc8e2d22d647487bd3a3102ad84aee5928
lateefat-taiwo <atandalateefat8@gmail.com>
2024-12-11 20:20:10 +0100

    update test for roles

--- tests/app/requests/roles.test.js
+++ tests/app/requests/roles.test.js
@@ -4,15 +4,16 @@ import request from 'supertest'
 import app from '../../../src/app'
 import DataService from '../../../src/app/services/DataService'
 import db from '../../../src/db/models'
-import {BAD_REQUEST, UNPROCESSABLE} from '../../../src/app/constants/statusCodes'
+import {BAD_REQUEST, OK, UNPROCESSABLE} from '../../../src/app/constants/statusCodes'
 import {INCOMPLETE_REQUEST, UNPROCESSABLE_REQUEST} from '../../../src/app/constants/messages'
-import {USER} from '../../../src/config/roles'
-import {fakeUser} from '../../fixtures'
+import {ADMIN, USER} from '../../../src/config/roles'
+import {adminUser, fakeUser} from '../../fixtures'
 
 const role = new DataService(db.Role)
 const user = new DataService(db.User)
 
 let userToken
+let adminToken
 
 describe('Roles Controller', () => {
   before(done => {
@@ -21,11 +22,21 @@ describe('Roles Controller', () => {
         user.create({...fakeUser, RoleId: record.id}).then(() => {
           request(app)
             .post('/api/auth/sign-in').send(fakeUser)
-            .end((_error, response) => {
+            .then((response) => {
               userToken = response.body.token
 
-              done()
-            })
+              role.create({name: ADMIN}).then(([adminRole]) => {
+                user.create({...adminUser, RoleId: adminRole.id}).then(() => {
+                  request(app)
+                  .post('/api/auth/sign-in').send(adminUser)
+                  .then((response) => {
+                    adminToken = response.body.token
+
+                    done()
+                  })
+                })
+              })
+          })
         })
       })
     })
@@ -61,6 +72,22 @@ describe('Roles Controller', () => {
 
           done()
         })
+    })
+  })
+
+  describe('GET /api/roles', () => {
+    it('successfully returns roles to admin user', done => {
+      request(app)
+      .get('/api/roles')
+      .set('Authorization', adminToken)
+      .end((error, response) => {
+        expect(error).to.not.exist
+        expect(response.statusCode).to.equal(OK)
+        expect(response.body.roles).to.have.lengthOf(2)
+        expect(response.body.roles.map(role => role.name)).to.deep.equal([USER, ADMIN])
+
+        done()
+      })
     })
   })
 })
