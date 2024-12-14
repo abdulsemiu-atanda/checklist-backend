5773fd2b2e71990923325581bc6177a0bcbb0d55
Abdulsemiu Atanda <semiu@live.com>
2024-12-14 16:47:54 -0400

    add subject to confirm user email

--- src/app/mailers/confirmUserEmail.js
+++ src/app/mailers/confirmUserEmail.js
@@ -2,7 +2,7 @@ import EmailBuilder from '../services/EmailBuilder'
 
 export default (user, code) => {
   const email = new EmailBuilder(
-    {to: user.email},
+    {to: user.email, subject: 'Account Confirmation'},
     [
       `Hi ${user.firstName}`,
       'Thanks for signing up to Checklist. Use the confirmation code below',
