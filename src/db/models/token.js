be49a128f63a68fdfe4dbcd7714f34bf0f66a742
Abdulsemiu Atanda <semiu@live.com>
2024-12-28 23:29:35 -0400

    setup token associations to invite

--- src/db/models/token.js
+++ src/db/models/token.js
@@ -20,6 +20,8 @@ export default (sequelize, DataTypes) => {
       )
 
       Token.belongsTo(models.User, {foreignKey: 'tokenableId', as: 'Collaborator', constraints: false})
+      Token.belongsTo(models.Invite, {foreignKey: 'tokenableId', as: 'Lead', constraints: false})
+      Token.hasOne(models.Invite, {foreignKey: 'tokenId'})
     }
   }
   Token.init({
@@ -52,12 +54,20 @@ export default (sequelize, DataTypes) => {
           for (const token of tokens) {
             if (token.tokenableType === 'User' && token.Collaborator !== undefined)
               token.tokenable = token.Collaborator
+            else if (token.tokenableType === 'Invite' && token.Lead !== undefined)
+              token.tokenable = token.Lead
 
             // To prevent mistakes:
             delete token.Collaborator
             delete token.dataValues.Collaborator
+            delete token.Lead
+            delete token.dataValues.Lead
           }
         }
+      },
+      async afterCreate(token) {
+        if (token.Invite)
+          await token.update({tokenableId: token.Invite.id, tokenableType: 'Invite'})
       }
     },
     sequelize,
