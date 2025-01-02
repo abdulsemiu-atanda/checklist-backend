b7b33597b29485497178eaabe2bb56c550500b4c
Abdulsemiu Atanda <semiu@live.com>
2025-01-02 03:40:56 -0400

    allow only owners to read,update and delete tasks

--- src/app/routes/tasks.js
+++ src/app/routes/tasks.js
@@ -2,14 +2,15 @@ import express from 'express'
 
 import controller from '../controllers/tasks'
 import middleware from '../middlewares/auth'
+import permissions from '../middlewares/permissions'
 
 const tasks = express.Router()
 
 tasks.route('/').post(middleware.isLoggedIn, controller.create)
 tasks.route('/').get(middleware.isLoggedIn, controller.index)
-tasks.route('/:id').get(middleware.isLoggedIn, controller.show)
-tasks.route('/:id').patch(middleware.isLoggedIn, controller.update)
+tasks.route('/:id').get(middleware.isLoggedIn, permissions.isOwner, controller.show)
+tasks.route('/:id').patch(middleware.isLoggedIn, permissions.isOwner, controller.update)
 tasks.route('/:id').put(middleware.isLoggedIn, controller.update)
-tasks.route('/:id').delete(middleware.isLoggedIn, controller.destroy)
+tasks.route('/:id').delete(middleware.isLoggedIn, permissions.isOwner, controller.destroy)
 
 export default tasks
