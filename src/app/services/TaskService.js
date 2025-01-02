3c34c9ee518a54945d8d69e42c7712d4107b5abe
Abdulsemiu Atanda <semiu@live.com>
2025-01-02 02:33:14 -0400

    add update method to task service

--- src/app/services/TaskService.js
+++ src/app/services/TaskService.js
@@ -3,8 +3,8 @@ import DataService from './DataService'
 import {redisKeystore} from '../../util/tools'
 import {decryptFields, encryptFields} from '../../util/cryptTools'
 
-import {CREATED, NOT_FOUND, OK, UNAUTHORIZED, UNPROCESSABLE} from '../constants/statusCodes'
-import {INCOMPLETE_REQUEST, RECORD_NOT_FOUND, UNPROCESSABLE_REQUEST} from '../constants/messages'
+import {CREATED, NOT_FOUND, OK, UNPROCESSABLE} from '../constants/statusCodes'
+import {RECORD_NOT_FOUND, UNPROCESSABLE_REQUEST} from '../constants/messages'
 import logger from '../constants/logger'
 
 class TaskService {
@@ -30,69 +30,76 @@ class TaskService {
 
   create({task, userId}, callback) {
     this.#session(userId).then(session => {
-      if (session) {
+      this.#userKey(userId).then(userKey => {
         const encryptor = new AsymmetricEncryptionService(session)
+        const encrypted = this.#encryptTask({record: task, encryptor, userKey})
 
-        this.#userKey(userId).then(userKey => {
-          const encrypted = this.#encryptTask({record: task, encryptor, userKey})
+        this.task.create({...encrypted, userId}).then(([record]) => {
+          const decrypted = this.#decryptTask({record: record.toJSON(), encryptor, userKey})
 
-          this.task.create({...encrypted, userId}).then(([record]) => {
-            const decrypted = this.#decryptTask({record: record.toJSON(), encryptor, userKey})
-
-            callback({status: CREATED, response: {data: decrypted, success: true}})
-          })
-        }).catch(error => {
-          logger.error(error.message)
-
-          callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: false}})
+          callback({status: CREATED, response: {data: decrypted, success: true}})
         })
-      } else {
-        callback({status: UNAUTHORIZED, response: {message: INCOMPLETE_REQUEST, success: false}})
-      }
+      }).catch(error => {
+        logger.error(error.message)
+
+        callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: false}})
+      })
     })
   }
 
   index(userId, callback) {
     this.#session(userId).then(session => {
-      if (session) {
-        const encryptor = new AsymmetricEncryptionService(session)
+      this.#userKey(userId).then(userKey => {
+        this.task.index().then(records => {
+          const encryptor = new AsymmetricEncryptionService(session)
+          const data = records.map(record => this.#decryptTask({record: record.toJSON(), encryptor, userKey}))
 
-        this.#userKey(userId).then(userKey => {
-          this.task.index().then(records => {
-            const data = records.map(record => this.#decryptTask({record: record.toJSON(), encryptor, userKey}))
-
-            callback({status: OK, response: {data, success: true}})
-          })
-        }).catch(error => {
-          logger.error(error.message)
-
-          callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: false}})
+          callback({status: OK, response: {data, success: true}})
         })
-      } else {
-        callback({status: UNAUTHORIZED, response: {message: INCOMPLETE_REQUEST, success: false}})
-      }
+      }).catch(error => {
+        logger.error(error.message)
+
+        callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: false}})
+      })
     })
   }
 
   show({id, userId}, callback) {
     this.#session(userId).then(session => {
-      if (session) {
-        this.#userKey(userId).then(userKey => {
-          const encryptor = new AsymmetricEncryptionService(session)
+      this.#userKey(userId).then(userKey => {
+        this.task.show({id}).then(record => {
+          if (record) {
+            const encryptor = new AsymmetricEncryptionService(session)
+            const decrypted = this.#decryptTask({record: record.toJSON(), encryptor, userKey})
 
-          this.task.show({id}).then(record => {
-            if (record) {
-              const decrypted = this.#decryptTask({record: record.toJSON(), encryptor, userKey})
-
-              callback({status: OK, response: {data: decrypted, success: true}})
-            } else {
-              callback({status: NOT_FOUND, response: {message: RECORD_NOT_FOUND, success: false}})
-            }
-          })
+            callback({status: OK, response: {data: decrypted, success: true}})
+          } else {
+            callback({status: NOT_FOUND, response: {message: RECORD_NOT_FOUND, success: false}})
+          }
         })
-      } else {
-        callback({status: UNAUTHORIZED, response: {message: INCOMPLETE_REQUEST, success: false}})
-      }
+      })
+    })
+  }
+
+  update({id, payload, userId}, callback) {
+    this.#session(userId).then(session => {
+      const {status, ...attributes} = payload
+
+      this.#userKey(userId).then(userKey => {
+        const encryptor = new AsymmetricEncryptionService(session)
+        const encrypted = this.#encryptTask({record: attributes, encryptor, userKey})
+        const task = status ? {...encrypted, status} : encrypted
+
+        this.task.update(id, task).then(record => {
+          const data = this.#decryptTask({record: record.toJSON(), encryptor, userKey})
+
+          callback({status: OK, response: {data, success: true}})
+        }).catch(error => {
+          logger.error(error.message)
+
+          callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: false}})
+        })
+      })
     })
   }
 }
