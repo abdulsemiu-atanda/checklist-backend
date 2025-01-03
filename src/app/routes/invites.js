7310fc8e5704a5b485ef7f3cec7556c5a310fda6
Abdulsemiu Atanda <semiu@live.com>
2025-01-03 16:18:33 -0400

    add invites for sending and resending invites

---
+++ src/app/routes/invites.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import controller from '../controllers/invites'
+import middleware from '../middlewares/auth'
+
+const invites = express.Router()
+
+invites.route('/:id/send').get(middleware.isLoggedIn, controller.send)
+invites.route('/:id/resend').get(middleware.isLoggedIn, controller.resend)
+
+export default invites
