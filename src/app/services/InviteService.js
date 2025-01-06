2aa6ef342680f53d912dbd6cdc8390ec6b986a2f
Abdulsemiu Atanda <semiu@live.com>
2025-01-05 20:33:21 -0400

    add method for accepting invites

--- src/app/services/InviteService.js
+++ src/app/services/InviteService.js
@@ -1,6 +1,6 @@
 import DataService from './DataService'
-import {dateToISOString, smtpServer} from '../../util/tools'
-import {digest} from '../../util/cryptTools'
+import {dateToISOString, redisKeystore, smtpServer} from '../../util/tools'
+import {digest, updatePrivateKey} from '../../util/cryptTools'
 import logger from '../constants/logger'
 import inviteEmail from '../mailers/inviteEmail'
 
@@ -10,6 +10,8 @@ import {SHARING} from '../../config/tokens'
 import {UNPROCESSABLE_REQUEST} from '../constants/messages'
 
 class InviteService {
+  #keystore = redisKeystore()
+
   constructor(models) {
     this.models = models
     this.smtp = smtpServer()
@@ -17,7 +19,11 @@ class InviteService {
     this.invite = new DataService(models.Invite)
     this.token = new DataService(models.Token)
     this.user = new DataService(models.User)
+    this.userKey = new DataService(models.UserKey)
+    this.sharedKey = new DataService(models.SharedKey)
+    this.permission = new DataService(models.Permission)
 
+    this.accept = this.accept.bind(this)
     this.resend = this.resend.bind(this)
     this.send = this.send.bind(this)
   }
@@ -70,6 +76,44 @@ class InviteService {
       }
 
       callback({status: ACCEPTED, response: {message: 'Invite resent', success: true}})
+    })
+  }
+
+  accept({id, currentUserId}, callback) {
+    this.models.sequelize.transaction(transaction => {
+      const changeOptions = {transaction}
+
+      this.#invite(id).then(invite => {
+        if (invite.Token.userId === currentUserId || invite.status !== PENDING) {
+          callback({status: ACCEPTED, response: {message: 'Invite accepted', success: true}})
+        } else {
+          this.user.show({id: currentUserId}).then(currentUser => {
+            if (currentUser.email === invite.email) {
+              this.#keystore.retrieve(currentUserId).then(session => {
+                this.invite.update(invite.id, {acceptedAt: dateToISOString(Date.now()), status: 'accepted'}, changeOptions)
+                this.permission.show({ownableId: invite.id, ownableType: 'Invite'}).then(permission => {
+                  this.permission.update(permission.id, {ownableId: currentUser.id, ownableType: 'User'}, changeOptions)
+                })
+
+                this.userKey.show({userId: invite.Token.userId}).then(parentKey => {
+                  const key = updatePrivateKey({backupKey: parentKey.backupKey, passphrase: session})
+
+                  this.sharedKey.create({key, userId: invite.Token.userId, ownableId: currentUser.id}, changeOptions)
+                    .then(() => {
+                      callback({status: ACCEPTED, response: {message: 'Invite accepted', success: true}})
+                    })
+                })
+              })
+            } else {
+              callback({status: ACCEPTED, response: {message: 'Invite accepted', success: true}})
+            }
+          })
+        }
+      }).catch(error => {
+        logger.error(error.message)
+
+        callback({status: UNPROCESSABLE, response: {message: UNPROCESSABLE_REQUEST, success: true}})
+      })
     })
   }
 
