6e6134bacef4410a1d0cc4681cda49be8be4bfc7
Abdulsemiu Atanda <semiu@live.com>
2025-01-10 19:58:08 -0400

    refactor fixture create helper

--- tests/fixtures/index.js
+++ tests/fixtures/index.js
@@ -3,28 +3,41 @@ import {fakerYO_NG as faker} from '@faker-js/faker'
 import DataService from '../../src/app/services/DataService'
 import db from '../../src/db/models'
 import {isEmpty} from '../../src/util/tools'
+import {adminUser, fakeUser} from './users'
 
 import {ADMIN, USER} from '../../src/config/roles'
+import AsymmetricEncryptionService from '../../src/app/services/AsymmetricEncryptionService'
+
+const defaultUser = trait => {
+  if (trait === ADMIN)
+    return adminUser
+  else
+    return {...fakeUser, email: faker.internet.email()}
+}
 
 const createUser = ({data, trait}) => {
-  const {fakeUser, adminUser} = require('./users')
+  const encryptor = new AsymmetricEncryptionService(data.password || fakeUser.password)
   const role = new DataService(db.Role)
   const user = new DataService(db.User)
+  const roleName = trait === ADMIN ? ADMIN : USER
 
-  if (trait === ADMIN) {
-    return role.create({name: ADMIN}).then(([record]) => {
-      const shared = {roleId: record.id}
-      const attributes = isEmpty(data) ? {...adminUser, ...shared} : {...data, ...shared}
+  switch(trait) {
+    case 'withUserKey':
+      return role.create({name: USER}).then(([record]) => {
+        const shared = {roleId: record.id}
+        const attributes = isEmpty(data) ? {...fakeUser, ...shared, email: faker.internet.email()} : {...data, ...shared}
 
-      return user.create(attributes)
-    })
-  } else {
-    return role.create({name: USER}).then(([record]) => {
-      const shared = {roleId: record.id}
-      const attributes = isEmpty(data) ? {...fakeUser, ...shared, email: faker.internet.email()} : {...data, ...shared}
+        return encryptor.generateKeyPair().then(({SHAFingerprint, ...keyPair}) => {
+          return user.create({...attributes, UserKey: {...keyPair, fingerprint: SHAFingerprint}}, {include: db.UserKey})
+        })
+      })
+    default:
+      return role.create({name: roleName}).then(([record]) => {
+        const shared = {roleId: record.id}
+        const attributes = isEmpty(data) ? {...defaultUser(trait), ...shared} : {...data, ...shared}
 
-      return user.create(attributes)
-    })
+        return user.create(attributes)
+      })
   }
 }
 
