e6e1d64c44acfa19916a5b447476568280ded922
Abdulsemiu Atanda <semiu@live.com>
2025-01-24 21:50:50 -0400

    add migration and model for tfa config

---
+++ src/db/migrations/20250125014218-create-tfa-config.js
@@ -0,0 +1,45 @@
+'use strict';
+const {ACTIVE, DISABLED, INITIAL} = require('../../config/tfaStatuses')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('TfaConfigs', {
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM(ACTIVE, DISABLED, INITIAL),
+        defaultValue: INITIAL
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('TfaConfigs');
+  }
+};
---
+++ src/db/models/tfaconfig.js
@@ -0,0 +1,33 @@
+import {Model} from 'sequelize'
+
+import {ACTIVE, DISABLED, INITIAL} from '../../config/tfaStatuses'
+
+export default (sequelize, DataTypes) => {
+  class TfaConfig extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      TfaConfig.belongsTo(models.User, {foreignKey: 'userId', onDelete: 'CASCADE'})
+    }
+  }
+  TfaConfig.init({
+    status: {
+      allowNull: false,
+      type: DataTypes.ENUM(ACTIVE, DISABLED, INITIAL),
+      defaultValue: INITIAL
+    },
+    url: DataTypes.STRING,
+    userId: {
+      allowNull: false,
+      type: DataTypes.UUID
+    }
+  }, {
+    sequelize,
+    modelName: 'TfaConfig',
+  })
+  return TfaConfig
+}
